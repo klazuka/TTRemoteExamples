4db7e170202124a1cf98209253199319ab6aca40
Keith Lazuka <klazuka@gmail.com>
2010-01-03 08:04:03 -0500

    Implemented a workaround for Three20's table cell image layout bug (thanks RoBak42 for the patch)
    There is a bug in Three20's table cell image logic w.r.t.
    Three20's image cache. By applying a TTImageStyle to the table item,
    we can override the layout logic to force the image to always be a fixed size.
    Otherwise, the image will sometimes overflow the bounds of the cell.
    
    This commit fixes the following issue:
    http://github.com/klazuka/TTRemoteExamples/issues#issue/1

--- Classes/SearchResultsTableDataSource.m
+++ Classes/SearchResultsTableDataSource.m
@@ -20,11 +20,23 @@
     
     // Construct an object that is suitable for the table view system
     // from each SearchResult domain object that we retrieve from the TTModel.
-    for (SearchResult *result in [(id<SearchResultsModel>)self.model results])
-        [self.items addObject:[TTTableImageItem itemWithText:result.title
-                                                    imageURL:result.thumbnailURL
-                                                defaultImage:[UIImage imageNamed:@"photo_placeholder.png"]
-                                                         URL:nil]];
+    for (SearchResult *result in [(id<SearchResultsModel>)self.model results]) {
+        TTTableImageItem* tii = [TTTableImageItem itemWithText:result.title
+                                                      imageURL:result.thumbnailURL
+                                                  defaultImage:[UIImage imageNamed:@"photo_placeholder.png"]
+                                                           URL:nil];
+
+        // There is a bug in Three20's table cell image logic w.r.t.
+        // Three20's image cache. By applying this TTImageStyle, we can
+        // override the layout logic to force the image to always be a fixed size.
+        // (thanks RoBak42 for the workaround!)
+        tii.imageStyle = [TTImageStyle styleWithImage:nil
+                                         defaultImage:[UIImage imageNamed:@"photo_placeholder.png"]
+                                          contentMode:UIViewContentModeScaleAspectFill
+                                                 size:CGSizeMake(75.f, 75.f)
+                                                 next:nil];
+        [self.items addObject:tii];
+    }
     
     TTLOG(@"Added %lu search result objects", (unsigned long)[self.items count]);
 }
